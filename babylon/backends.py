8f0392ad522be693b5cfd658d3c3261546b7fe91
Thomas Schroeter <thomas@cliqz.com>
2014-10-25 14:19:39 +0200

    Entity backend part 1

--- babylon/backends.py
+++ babylon/backends.py
@@ -1,7 +1,13 @@
-import redis
-import urlparse
+import datetime
 import os
 import pymongo
+import redis
+import urlparse
+from bson.objectid import ObjectId
+
+import article
+import person
+import tag
 
 
 redis_url = os.environ['REDISCLOUD_URL']
@@ -17,6 +23,67 @@ mongo_conn = pymongo.MongoClient(mongo_url)
 
 MONGO_DB_NAME = 'babylon'
 
+ARTICLES_COLLECTION = 'articles'
+PERSONS_COLLECTION = 'persons'
+TAGS_COLLECTION = 'tags'
+
+db = mongo_conn[MONGO_DB_NAME]
+articles = db[ARTICLES_COLLECTION]
+persons = db[PERSONS_COLLECTION]
+tags = db[TAGS_COLLECTION]
+
+def time():
+    return datetime.datetime.utcnow().isoformat()
+
+def link(collection, id1, relation, id2):
+    oid1 = ObjectId(id1)
+    collection.update({'_id': oid1}, {'$addToSet': {relation: id2}})
+
+def get_article(url):
+    return get_or_create(articles, {"source_url": url}, article.template)
+
+def store_article(article):
+    articles.save(with_mongo_id(article))
 
 
+def get_person(handle):
+    return get_or_create(persons, {"handle": handle}, person.template)
 
+def store_person(person):
+    persons.save(with_mongo_id(person))
+
+
+def get_tag(name):
+    return get_or_create(tags, {"name": name}, tag.template)
+
+def store_tag(tag):
+    tags.save(with_mongo_id(tag))
+
+
+def with_mongo_id(obj):
+    obj['_id'] = ObjectId(obj['id'])
+    return obj
+
+def get_or_create(collection, spec, template):
+    a = collection.find_one(spec)
+    if not a:
+        assert len(spec.values()) == 1
+        a = template(spec.values()[0])
+        oid = ObjectId()
+        a['_id'] = oid
+        a['id'] = unicode(oid)
+        collection.save(a)
+    return json_ready(a)
+
+def json_ready(article):
+    article.update(dict(id=unicode(article['_id']), meta=get_meta()))
+    article.pop('_id')
+    return article
+
+def get_meta():
+    return {"meta": {
+        "people": [],
+        "topics": [],
+        "tags": []
+        }
+    }
--- tests/test_backend.py
+++ tests/test_backend.py
@@ -1,9 +1,34 @@
 from babylon import backends
 
 
-def test_user_type():
-    assert 1 == 2
+def test_article_get_or_create():
+    url = 'fun'
+    a = backends.get_article(url)
 
+    assert a['title'] == ''
+    assert a['source_url'] == url
+
+def test_store_article():
+    url = 'fun'
+    assert backends.articles.find({'url': url}).count() == 0
+    a = backends.get_article(url)
+    backends.store_article(a)
+    a = backends.get_article(url)
+    assert a['source_url'] == 'fun'
+
+def test_person_get_or_create():
+    handle = 'h'
+    assert backends.persons.find({'handle': handle}).count() == 0
+    p = backends.get_person(handle)
+    assert p['handle'] == handle
+    assert 'meta' in p
+
+def test_link():
+    a = backends.get_article('a')
+    t = backends.get_tag('t')
+    backends.link(backends.articles, a['id'], 'tags', t['id'])
+    a = backends.get_article('a')
+    assert a['tags'] == [t['id']]
 
 def setup_module(module):
     backends.redis_conn.flushall()
